174ad1a2b9dab2f915ab0c6cbcd1b0d7bfcc2667
LOVECHEN <softnote@gmail.com>
2026-09-07 02:22:03 +0800

    backfill(7.24.9): 上游7.24.9源码+去广告+修好的构建工具，用于补建正式版

--- v2rayN/ServiceLib/Global.cs
+++ v2rayN/ServiceLib/Global.cs
@@ -645,7 +645,7 @@ public class Global
         { ECoreType.overtls, "ShadowsocksR-Live/overtls" },
         { ECoreType.shadowquic, "spongebob888/shadowquic" },
         { ECoreType.mieru, "enfein/mieru" },
-        { ECoreType.v2rayN, "2dust/v2rayN" },
+        { ECoreType.v2rayN, "LOVECHEN/v2rayN-mod" },
     };
 
     public static readonly List<string> OtherGeoUrls =
